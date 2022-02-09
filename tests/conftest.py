6843357b2845d586d9e4e509503f41343783a263
IGor <79050023+Ig0r-gor@users.noreply.github.com>
2022-02-10 02:55:28 +0400

    Add files via upload

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,13 @@
 import pytest
 from selenium import webdriver
-import time
+
 
 @pytest.fixture(scope="function")
 def driver(request):
     path = str(request.fspath)
-    # print(request.fspath)
-    # print(f"{path[:path.find('tests')]}\chromedriver")
-    # time.sleep(5)
+
     driver = webdriver.Chrome(f"{path[:path.find('tests')]}chromedriver")
     driver.maximize_window()
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(10)
     yield driver
     driver.close()
